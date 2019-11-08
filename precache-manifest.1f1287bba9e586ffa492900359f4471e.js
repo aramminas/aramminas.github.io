22ff26486acf46cbeb2bf894f647bed054de20c2
Aram <armfiletransfer@gmail.com>
2019-11-08 19:41:37 +0400

    added CV project in github site

---
+++ precache-manifest.1f1287bba9e586ffa492900359f4471e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0105eb5ef855f9f1fa82610bc7abc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6463256f97242f9369e",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1a6f2c0178e364e9df07",
+    "url": "/static/css/main.c63e9d55.chunk.css"
+  },
+  {
+    "revision": "b6463256f97242f9369e",
+    "url": "/static/js/2.e56271d7.chunk.js"
+  },
+  {
+    "revision": "1a6f2c0178e364e9df07",
+    "url": "/static/js/main.27430a07.chunk.js"
+  },
+  {
+    "revision": "2df4d280065006baff63",
+    "url": "/static/js/runtime-main.889f1852.js"
+  }
+]);
